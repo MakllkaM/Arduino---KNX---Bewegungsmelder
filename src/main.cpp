1813120685db87ab1099b16343a2980a98a14e57
MakllkaM <31666143+MakllkaM@users.noreply.github.com>
2024-06-26 15:01:21 +0200

    time selection integratet

--- src/main.cpp
+++ src/main.cpp
@@ -11,42 +11,64 @@
  *
  */
 
- // define PINS
- #define _i_motionDetect    12
+// define PINS
+#define _i_motionDetect         12
+#define _b_shortSelection      11      
+#define _b_normalSelection     10
+#define _b_longSelection        9
 
- // create Variables
- int l_i_motion             = false;            // local variable fo Integer
- int l_b_motionSave         = false;
+// create Variables
+int _i_motion             = false;            // local variable fo Integer
+int _b_motionSave         = false;
 
- unsigned long l_ul_previousMillis = 0;  // will store last time LED was updated
 
- // constants won't change:
- const long c_l_interval = 5000;                    // interval at which to blink (milliseconds)
- 
+
+unsigned long _ul_previousMillis = 0;  // will store last time LED was updated
+
+// constants won't change:
+const long c_l_shortDelay       = 5000;                    // interval at which to blink (milliseconds)
+const long c_l_normalDelay      = 30000;
+const long c_l_standardDelay    = 15000;  
+const long c_l_longDelay        = 60000;
+
+long delayTime(){
+    if (digitalRead(_b_shortSelection) == 1){
+        return c_l_shortDelay;
+    }else if (digitalRead(_b_snormalSelection) == 1){
+        return c_l_normalDelay;
+    }else if (digitalRead(_b_longSelection, HIGH) == 1){
+        return c_l_longDelay;
+    }else{
+        return c_l_standardDelay;
+    }
+}
 
 void setup()
  {
     Serial.begin(9600);
 
     pinMode(_i_motionDetect, INPUT);
+    pinMode(_b_shortSelection, INPUT);
+    pinMode(_b_normalSelection, INPUT);
+    pinMode(_b_longSelection, INPUT);
  }
 
  void loop()
  {
-    unsigned long l_ul_currentMillis = millis(); 
+    unsigned long _ul_currentMillis = millis(); 
 
-    l_i_motion = digitalRead(_i_motionDetect);
+    _i_motion = digitalRead(_i_motionDetect);
 
-    if (l_i_motion == HIGH){
-        if (l_b_motionSave == false){
+    if (_i_motion == HIGH){
+        if (_b_motionSave == false){
             Serial.println("true");
         }
-        l_b_motionSave = true;
-        l_ul_previousMillis = l_ul_currentMillis;
+        _b_motionSave = true;
+        _ul_previousMillis = _ul_currentMillis;
     }
 
-    if (l_ul_currentMillis - l_ul_previousMillis >= c_l_interval and l_b_motionSave){
-        l_b_motionSave = false;
+    if (_ul_currentMillis - _ul_previousMillis >= delayTime() and _b_motionSave){
+        _b_motionSave = false;
         Serial.println("false");
     }
  
